b56949d8ce92dfd2f15b9883b35ac7b5c460c2d7
Felix Paradis <allo@felixparadis.com>
2021-04-18 22:52:37 -0400

    dreamweaver version

---
+++ scripts/email.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+function stringReverse(textString){
+    if(!textString)return'';
+    var revString='';
+        for (i = textString.length -1; i>=0; i--)
+        revString+=textString.charAt(i);
+    return revString;
+       
+}
+userName = stringReverse("reldac");
