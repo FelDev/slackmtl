4fa30cd12ec352d8fafef524feac3e32d93ea039
Felix Paradis <allo@felixparadis.com>
2021-09-19 14:08:26 -0500

    basic gatsby setup

---
+++ gatsby-config.js
@@ -0,0 +1,48 @@
+require('dotenv').config({
+  path: '.env',
+})
+console.log('@process.env.SHOPIFY_ADMIN_PASSWORD: ', process.env.SHOPIFY_ADMIN_PASSWORD)
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "slacklinemtl",
+  },
+  plugins: [{
+      resolve: "gatsby-source-shopify",
+      options: {
+        // accessToken: ,
+        password: process.env.SHOPIFY_ADMIN_PASSWORD,
+        storeUrl: "slacklinemtl.myshopify.com",
+      },
+    },
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-12707426-2",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
