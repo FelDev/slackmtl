9d7bd45d6ce42475c479e6fd9965d2cec87558d8
Felix Paradis <allo@felixparadis.com>
2021-09-19 15:18:29 -0500

    remove preact as it fails on build

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,14 @@ module.exports = {
     DEV_SSR: false,
   },
   plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    // `gatsby-plugin-preact`,
+    'gatsby-plugin-no-sourcemaps',
     {
       resolve: 'gatsby-source-shopify',
       options: {
@@ -18,20 +26,12 @@ module.exports = {
         storeUrl: 'slacklinemtl.myshopify.com',
       },
     },
-    'gatsby-plugin-image',
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-12707426-2',
       },
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-mdx',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    `gatsby-plugin-preact`,
-    'gatsby-plugin-no-sourcemaps',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
